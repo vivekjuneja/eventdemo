22f60945699c15e1d5d8b5fbe246d206fc089464
Vivek Juneja <vivekjuneja@gmail.com>
2015-05-21 20:37:45 +0900

    Updated API Implementation

--- src/main/java/org/juneja/eventdemo/api/impl/ProductPurchaseAPI.java
+++ src/main/java/org/juneja/eventdemo/api/impl/ProductPurchaseAPI.java
@@ -1,5 +1,23 @@
 package org.juneja.eventdemo.api.impl;
 
-public class ProductPurchaseAPI {
+import org.juneja.eventdemo.entity.Response;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ProductPurchaseAPI {
+	
+	@RequestMapping("/products/{id}/purchase")
+	public Response purchaseProduct(@PathVariable(value="id") String id) {
+		return new Response("1", "Ok Works !", "200 OK");
+	}
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
 }
+
+

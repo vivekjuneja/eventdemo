b3c3d3f74f8a3a2c2bba62e78d3d6a4ad08e57ce
Vivek Juneja <vivekjuneja@gmail.com>
2015-05-23 22:22:28 +0900

    Removed reference to ProductRepository

--- src/main/java/org/juneja/eventdemo/api/impl/ProductPurchaseAPIApp.java
+++ src/main/java/org/juneja/eventdemo/api/impl/ProductPurchaseAPIApp.java
@@ -1,7 +1,5 @@
 package org.juneja.eventdemo.api.impl;
 
-import org.juneja.eventdemo.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
@@ -11,8 +9,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ProductPurchaseAPIApp  {
 
-	@Autowired
-	private static ProductRepository repository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductPurchaseAPIApp.class, args);
